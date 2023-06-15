a878c185733d73816e8bdf6c7caef9129d38245e
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-15 11:12:55 -0700

    taskadv2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@ int main(void)
 {
 	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, sentence, sizeof(sentence));
+	fwrite(2, sentence, sizeof(sentence));
 	return (1);
 }
