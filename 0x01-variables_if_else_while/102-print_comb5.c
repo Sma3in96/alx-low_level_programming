ff13d06e9769efe236fadace1699875f93335159
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-16 07:33:35 -0700

    taskadv3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ *Return: always (0)
+ */
+int main(void)
+{
+	int a = 48;
+
+	while ( a < 58 )
+	{
+		int b =  a;
+
+		while ( b < 58 )
+		{
+			int c = 48;
+
+			while ( c < 58 )
+			{
+				int d = 48;
+
+				while ( d < 58)
+				{
+					putchar(32);
+					putchar(a);
+					putchar(b);
+					putchar(32);
+					putchar(c);
+					putchar(d);
+					putchar(44);
+				d++;
+				}
+			c++;
+			}
+		b++;
+		}
+	a++;
+	}
+}
