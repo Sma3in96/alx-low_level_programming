5b87769be550b900e1857cf0d510da3e2e1708f5
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-16 04:32:41 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		putchar(a);
+		a++;
+	}
+	a = 97;
+	while (a < 102)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
