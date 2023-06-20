2a05d46d66a475460569c8a542b008ba8eb28b63
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-20 04:39:49 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_sign - return abs value of a given number
+ * @n: interge to test
+ * Return: abs value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return -n;
+	}
+	else
+	{
+		return n;
+	}
+}
