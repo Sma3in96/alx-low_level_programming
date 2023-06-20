82d5a6140946331ae2a36d24cc0ffe816599f491
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-20 03:26:25 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabetx10 - this function print alpha lowercases 10 times
+ *
+ * Return: this function return void
+ */
+void print_alphabet_x10(void)
+{
+	int index, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (index = 97; index < 123; index++)
+		{
+			_putchar(index);
+		}
+	}
+	_putchar('\n');
+}
