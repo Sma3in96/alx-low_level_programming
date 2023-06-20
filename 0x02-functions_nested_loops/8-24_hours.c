6cc45ae93d3f900f77aa97e29b96820f3d709d91
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-20 06:13:34 -0700

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - print hours
+ *
+ * Return: None.
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 3; a++)
+	{
+		for (b = 0; b < 4; b++)
+		{
+			for (c = 0; c < 6; c++)
+			{
+				for (d = 0; d < 10; d++)
+				{
+					_putchar(48 + a);
+					_putchar(48 + b);
+					_putchar(58);
+					_putchar(48 + c);
+					_putchar(48 + d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
