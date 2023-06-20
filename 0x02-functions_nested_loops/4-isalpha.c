752c4d206450544ee5e196390e7b046e2ff86b1b
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-20 04:05:31 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabet
+ * @c: the code ascii of the letter we want to check
+ *
+ * Return: 1 if true 0 if false
+ */
+int _isalpha(int c)
+{
+	if (((c > 96) && (c < 123) || (c > 64) && (c < 91)))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
