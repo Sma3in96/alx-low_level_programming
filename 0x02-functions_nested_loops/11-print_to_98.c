8b1fa575d841637f4b2de7ab1210d0cea8310fb0
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-20 09:41:37 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -30,8 +30,16 @@ void print_to_98(int n)
 	{
 		for ( i = n; i > 97; i--)
 		{
-			printf("%d, ", i);
+			if (i == 98)
+			{
+				printf("%d, ", i);
+				continue;
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
 		}
-		_putchar('\n');
+		printf("\n");
 	}
 }
