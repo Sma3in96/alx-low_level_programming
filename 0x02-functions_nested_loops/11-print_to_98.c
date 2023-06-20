8e21faf2a5fd4804ab41f8db1b90328a1f84bec9
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-20 09:22:00 -0700

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_to_98 - print from a int to 98
+ * @n: starting point
+ *
+ * Return: None.
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for ( i = n; i < 99; i++)
+		{
+			printf("%d", i);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for ( i = n; i > 97; i--)
+		{
+			printf("%d", i);
+		}
+		_putchar('\n');
+	}
+}
