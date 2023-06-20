72e8f86909a0d4d7c48e45bfddec4196dedc2581
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-20 16:32:22 -0700

    taskadv2

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints  numbers below 1024
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ao, sum;
+
+	for (ao = 1; ao < 1024; ao++)
+	{
+		if ((ao % 5) == 0 || (ao % 3) == 0)
+			sum += ao;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
