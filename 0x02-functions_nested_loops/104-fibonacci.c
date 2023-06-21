016420b8cbfc94617a167a793ae94a0ea4ae2db4
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-20 17:47:18 -0700

    tasklast

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Prints  numbers below 1024
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	long int sum;
+	long int d1 = 1;
+	long int d2 = 2;
+
+	printf("%ld, %ld", d1, d2);
+	for (i = 0; i < 96; i++)
+	{
+		sum = d1 + d2;
+		printf(", %ld", sum);
+		d1 = d2;
+		d2 = sum;
+	}
+	printf("\n");
+	return (0);
+}
