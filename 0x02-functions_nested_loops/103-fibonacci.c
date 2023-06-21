733529a30a313ee59221944f54347582a975d2d7
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-20 17:19:08 -0700

    taskadv3

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Prints  numbers below 4m
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int new;
+	long int sum = 2;
+	long int d1 = 1;
+	long int d2 = 2;
+
+	while (new < 4000000)
+	{
+		new = d1 + d2;
+		if (new % 2 == 0)
+			sum += new; 
+		d1 = d2;
+		d2 = new;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
