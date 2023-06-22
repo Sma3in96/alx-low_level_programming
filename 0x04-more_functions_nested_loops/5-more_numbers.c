d54382bb1d6f141c2f64f6a76b56596a91501d4d
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-22 03:43:26 -0700

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers with putchar from 0 to 14
+ *
+ * Return: nothing at all
+ */
+
+void more_numbers(void)
+{
+	int i, int j = 0, int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (j == 1)
+			{
+				_putchar(49);
+			}
+			_putchar(48 + (i % 10));
+			if (i == 9)
+				j = 1;
+		}
+	_putchar('\n');
+	}
+}
