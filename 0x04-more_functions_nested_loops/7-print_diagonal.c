5ccf1840ddcafa665bde71d40ed83cca6a32cddc
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-22 04:56:14 -0700

    task6

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print antislash and pretend they are diagonals
+ * @n: the number
+ *
+ * Return: None.
+ */
+void print_diagonal(int n)
+{
+	if (n > O)
+	{
+		int i;
+		int j = 0;
+
+		for (i = 0; i < n; i++)
+		{
+			while (j < i)
+			{
+				_putchar(32);
+				j++;
+			}
+			_putchar(92);
+			j = 0;
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n')
+	}
+}
