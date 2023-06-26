80a5d6fc7831ff907ee1bc402ac555a235e12775
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-26 16:16:04 -0700

    taskadv1

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoi - return int inside of string
+ * @s: string
+ *
+ * Return: the integer
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int locksign = 1;
+	int lockchar = 0;
+	int res = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + 1) == 43 && locksign)
+			sign *= 1;
+		if (*(s + 1) == 45 && locksign)
+			sign *= -1;
+		if ((*(s + i) >= 48) && (*(s + i) <= 57))
+		{
+			res = res*10 + (*(s + i) - 48 );
+			locksign = 0;
+			locksign = 1;
+		}
+		if (((*(s + i) < 48) || (*(s + i) > 57)) && lockchar)
+			break;
+		i++;
+	}
+	return (res * sign);
+}
