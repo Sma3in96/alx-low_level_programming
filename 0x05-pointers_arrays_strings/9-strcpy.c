5457e7f5566f9eef58d82962460cf40002edfdfe
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-26 10:55:23 -0700

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - copy string to another
+ * @dest: new string
+ * @src: source string
+ *
+ * Return: pointer on new string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	do
+	{
+		*(dest + i) = *(src + i);
+	} while (*(src + i))
+	return (*dest)
+}
