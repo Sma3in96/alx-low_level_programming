b4f806f3a14c09fd6bdcc1f50ae38d137b36e6fe
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-29 15:08:12 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - upper a str
+ * @str: string
+ *
+ * Return: str after upper
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		{
+			*(str + i) -= 32;
+		}
+		i++;
+	}
+}
