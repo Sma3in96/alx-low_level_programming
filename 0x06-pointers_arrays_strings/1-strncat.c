bc4c0601cf628efd66c329a8ff0c107174bdcbcf
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-29 13:36:57 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - concatenat 2 strs
+ * @dest: dest
+ * @src: src
+ * @n: int
+ *
+ * Return: pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(dest + i))
+	{
+	i++;
+	}
+	while (*(src + j) && j < n)
+	{
+	*(dest + i + j) = *(src + j);
+	j++;
+	}
+	return (dest);
