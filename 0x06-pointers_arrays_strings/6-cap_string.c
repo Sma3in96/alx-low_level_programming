390cf81fc7adaeaaac1ce6416a39e26e8b7e3fc2
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-29 16:47:17 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,23 +7,24 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0, newword = 1;
+	int i = 0, j;
+	int nwword[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
+	if (*(s + i) <= 'z' && *(s + i) >= 'a')
+		*(s + 1 + i) = *(s + 1 + i) - 32;
 	while (*(s + i))
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z' && newword == 1)
+		for (j = 0; j <= 12; j++)
 		{
-			*(s + i) = *(s + i) - 32;
-			newword = 0;
+			if nwword[j] == *(s + i)
+			{
+				if (*(s + 1 + i) <= 'z' && *(s + 1 + i) >= 'a')
+				{
+					*(s + 1 + i) = *(s + 1 + i) - 32;
+					break;	 
+				}
+			}
 		}
-		if (*(s + i) == 32 || *(s + i) == 9 || *(s + i) == 10 || *(s + i) == 44)
-			newword = 1;
-		else if (*s + i) == 46 || *(s + i) == 33 || *(s + i) == 63 || *(s + i) == 34)
-			newword = 1;
-		else if (*(s + i) == 41 || *(s + i) == 123 || *(s + i) == 125 || *(s + i) == 40)
-			newword = 1;
-		else if (*(s + i) == 59)
-			newword = 1;
 		i++;
 	}
 	return (s);
