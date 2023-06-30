b07687d166e122579d0e15eb31d4e3c8b280763f
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-29 17:29:12 -0700

    task7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - entry point
+ * @s: string
+ *
+ * Return: pointer
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+	int low[] = {97, 101, 111, 116, 108};
+	int upper[] = {65, 69, 79, 84, 76};
+	int subs[] = {52, 51, 48, 55, 49};
+
+	while (*(s + i))
+	{
+		for (j = 0; j <= 4; j++)
+		{
+			if (*(s + i) == low[j] || *(s + i) == upper[j])
+			{
+				*(s + i) = subs[j];
+				break;
+			}			
+		}
+		i++;
+	}
+}
