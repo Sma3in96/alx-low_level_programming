ac7b877fbdec22c30734ab9a94253fd21d623d01
Sma3in96 <oumouss.ismail5@gmail.com>
2023-06-30 14:03:38 -0700

    task101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,32 @@
+#include  "main.h"
+/**
+ * print_number -  print numbers.
+ * @n: the integer.
+ *
+ * Return: none.
+ */
+void print_number(int n)
+{
+	unsigned int i, j, exp;
+
+	exp = 1;
+	if (n < 0)
+	{
+		i = -1 * n;
+		_putchar(45);
+	}
+	else
+	{
+		i = n;
+	}
+	j = i;
+	while (j > 9)
+	{
+		exp = exp * 10;
+		j = j / 10;
+	}
+	for (; exp >= 1; exp /= 10)
+	{
+		_putchar(48 + ((i / exp) % 10));
+	}
+}
