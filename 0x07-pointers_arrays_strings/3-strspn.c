671f7ae3ac2db39486b9ab4674de48516808dec7
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-03 07:29:50 -0700

    task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - number of byte
+ * @s: string
+ * @accept: chars
+ *
+ * Return: int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, sum = 0;
+
+	while (*s != '\0')
+	{
+		for (i = 0; *(accept + i) != '\0'; i++)
+		{
+			if (*s == *(accept + i))
+			{
+				sum++;
+				break;
+			}
+		}
+		s++;
+	}
+	return (sum);
+}
