6dc51a20d8b78eecb06ce2a2fb543ce222376d48
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-03 16:09:06 -0700

    task7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio>
+/**
+ * print_diagsums - sum of diags
+ * @a: array
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	unsigned int i, j, sumd1 = 0, sumd2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sumd1 += a[i][j];
+			}
+			if (i + j == size - 1)
+			{
+				sumd2 += a[i][j];
+			}
+		}
+	}
+	printf("%d,%d\n",sumd1 ,sumd2);
+}
