87ddff883cd74bd58fc06c3418517c982452196a
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-03 07:14:31 -0700

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,19 +9,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	int found = 0;
-
-	if (*s == '\0')
-		return (s);
-	while (*++s != '\0')
+	while (*s != '\0')
 	{
 		if (*s == c)
 		{
-			found = 1;
-			break;
+			return (s);
 		}
+		s++;
 	}
-	if (found == 0)
-		s = NULL;
-	return (s);
+	if (*s == c)
+		return (s);
+	return (NULL);
 }
