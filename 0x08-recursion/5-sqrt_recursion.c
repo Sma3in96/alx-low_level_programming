ebbfa5fad10545b4ae66679f92bf11e7e75ef4a2
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-05 06:11:00 -0700

    task6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - return sqrt
+ * @n: int
+ *
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (i * i == n)
+		return (i);
+
+	_sqrt_recursion(i - 1);
+	return (-1);
+}
