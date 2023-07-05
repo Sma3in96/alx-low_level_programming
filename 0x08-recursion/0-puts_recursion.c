5764d5f11732e31819364c86919e43b707f17287
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-05 03:38:07 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,8 +7,16 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	_putchar(*s++);
-	return (_puts_recursion(*s));
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+		
+	
 }
