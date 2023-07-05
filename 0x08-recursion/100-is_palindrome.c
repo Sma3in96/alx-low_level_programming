55146260fb5fcea06671ab08c2963aab3dbce76a
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-05 15:23:38 -0700

    taskadv

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,7 +10,7 @@ int length(char *s, int i);
 int check(char *s, int index, int l);
 int is_palindrome(char *s)
 {
-	return (check(s, 0, length(s, 0));
+	return (check(s, 0, length(s, 0)));
 }
 int length(char *s, int i)
 {
@@ -20,7 +20,7 @@ int length(char *s, int i)
 }
 int check(char *s, int index, int l)
 {
-	if (*(s + index) != *(s + (l - 1 - index))
+	if (*(s + index) != *(s + (l - 1 - index)))
 		return (0);
 	else
 		return (1 * check(s, index + 1, l); 
