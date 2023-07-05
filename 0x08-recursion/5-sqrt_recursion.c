62b9969dd6e1e9fa06a54e9bf18ff08033a120b8
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-05 14:43:11 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,16 +2,29 @@
 /**
  * _sqrt_recursion - return sqrt
  * @n: int
+ * @p: int
  *
  * Return: int
  */
+int look(int n, int mul);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (i * i == n)
-		return (i);
-
-	_sqrt_recursion(i - 1);
+	return (look(n, 1));
+}
+/**
+ * look - looking for mul
+ * @n: int
+ * @p: int
+ *
+ * Return: mul
+ */
+int look(int n, int mul)
+{
+	if (mul * mul == n)
+		return (mul);
+	if (mul < n)
+		return (look(n, mul + 1));
 	return (-1);
 }
