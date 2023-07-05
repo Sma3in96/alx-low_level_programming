cea205eb53e4e9a4da7757b508a045706ca912a1
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-05 15:45:50 -0700

    taskadv

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,7 +2,7 @@
 /**
  * is_palindrome - check if string is palindrome
  * @s: string
- *
+ * @i: index
  *
  * Return: int
  */
@@ -12,12 +12,25 @@ int is_palindrome(char *s)
 {
 	return (check(s, 0, length(s, 0)));
 }
+/**
+ * length - lenght
+ * @s: string
+ * @i: int
+ * Return: 1
+ */
 int length(char *s, int i)
 {
 	if (*(s + i) == '\0')
 		return (0);
 	return (1 + length(s, i + 1));
 }
+/**
+ * check - check for char
+ * @s: string
+ * @index: i
+ * @l: lenght
+ * Return: int
+ */
 int check(char *s, int index, int l)
 {
 	if (index == l / 2)
@@ -25,6 +38,6 @@ int check(char *s, int index, int l)
 	if (*(s + index) != *(s + (l - 1 - index)))
 		return (0);
 	else
-		return (1 * check(s, index + 1, l)); 
+		return (1 * check(s, index + 1, l));
 
 }
