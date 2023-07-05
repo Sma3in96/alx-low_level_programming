17d00f7361d4db7c6ef015317540322086257229
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-05 15:18:58 -0700

    taskadv

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * is_palindrome - check if string is palindrome
+ * @s: string
+ *
+ *
+ * Return: int
+ */
+int is_palindrome(char *s)
+{
+	return (check(s, 0, length(s, 0));
+}
+int length(char *s, int i)
+{
+	if (*(s + i) != '\0')
+		return (0);
+	return (1 + length(s, i + 1));
+}
+int check(char *s, int index, int l)
+{
+	if (*(s + index) != *(s + (l - 1 - index))
+		return (0);
+	else
+		return (1 * check(s, index + 1, l); 
+
+}	
