5ceba676706f945c63fa20d16a509e0db38882b4
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-07 05:14:40 -0700

    taskadv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * count - count the change
+ * @arg: int
+ * Return: coins
+ */
+int count(int arg)
+{
+	int coins = 0;
+
+	while (arg > 0)
+	{
+		if (arg >= 25)
+		{
+			coins = coins + arg / 25;
+			arg = arg % 25;
+			continue;
+		}
+		if (arg >= 10)
+		{
+			coins = coins + arg / 10;
+			arg = arg % 10;
+			continue;
+		}
+		if (arg >= 5)
+		{
+			coins = coins + arg / 5;
+			arg = arg % 5;
+			continue;
+		}
+		if (arg >= 2)
+		{
+			coins = coins + arg / 2;
+			arg = arg % 2;
+			continue;
+		}
+		if (arg >= 1)
+		{
+			coins++;
+			arg = arg - 1;
+			continue;
+		}
+	return (coins);
+}
+/**
  * main - print given to the program
  * @argc: number of arg
  * @argv: array of arg
@@ -8,7 +51,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int coins = 0, arg;
+	int coin = 0, p;
 
 	if (argc != 2)
 	{
@@ -17,41 +60,9 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		arg = atoi(argv[1]);
-		while (arg > 0)
-		{
-			if (arg >= 25)
-			{
-				coins = coins + arg / 25;
-				arg = arg % 25;
-				continue;
-			}
-			if (arg >= 10)
-			{
-				coins = coins + arg / 10;
-				arg = arg % 10;
-				continue;
-                        }
-			if (arg >= 5)
-                        {
-                                coins = coins + arg / 5;
-				arg = arg % 5;
-				continue;
-                        }
-			if (arg >= 2)
-			{
-				coins = coins + arg / 2;
-				arg = arg % 2;
-				continue;
-			}
-			if (arg >= 1)
-                        {
-				coins++;
-				arg = arg - 1;
-				continue;
-                        }		
-		}
-		printf("%d\n", coins);
+		p = atoi(argv[1]);
+		coin = count(p);
+		printf("%d\n", coin);
 	}
 	return (0);
 }
