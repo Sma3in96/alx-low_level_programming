177029c1a992fedb6ec2961fdff38dc524ad3bdc
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-07 02:17:07 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: number of arg
+ * @argv: array of arg
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
