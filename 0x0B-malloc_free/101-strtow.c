c36f1463911d879f8cb7c531c485c2f7f44ead54
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-10 09:39:59 -0700

    taskadv2

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,7 +10,7 @@ char **strtow(char *str)
 	int j = 0, wcount = 0, flag = 0, i = 0, n = 0, k = 0;
 	char **p;
 
-	if (*str == "" || *str == " ")
+	if (str[0] == '' || str[0] == ' ')
 		return (NULL);
 	while (*(str + j) != '\0')
 	{
