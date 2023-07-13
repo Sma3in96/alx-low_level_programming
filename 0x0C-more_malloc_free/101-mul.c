3f58b2589ea5b677018edddda135e9435087e3a8
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-13 15:35:34 -0700

    taskadv

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("error\n");
+		exit(98);
+	}
+	agrv = agrv;
+	return (0);
+}
