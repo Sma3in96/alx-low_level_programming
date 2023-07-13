730d63f1139af20c4f5fec037692a29fb5924cfe
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-13 05:11:08 -0700

    task0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,13 +3,14 @@
 /**
  * malloc_checked - allocate a memory space
  * @b: size to allocate
+ *
  * Return: void
  */
 void *malloc_checked(unsigned int b)
 {
 	char *p;
 
-	p = malloc(b);
+	p = malloc(b * sizeof(char));
 	if (p == NULL)
 		exit(98);
 	return (p);
