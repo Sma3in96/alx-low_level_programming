8f409f02decd1b7902e39becd113491f1a38a2a0
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-13 14:50:56 -0700

    taskadv

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - reallocate memory
+ * @ptr: array
+ * @old_size: int
+ * @new_size: int
+ * Return: void
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	char *ptrc = (char *)ptr;
+	unsigned int i;
+
+	if (old_size == new_size)
+	{
+		p = ptrc;
+		free(ptr);
+		return (p);
+	}
+	if (ptr == NULL)
+		return (malloc(new_size * sizeof(char)));
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	p = malloc(new_size * sizeof(char));
+	for (i = 0; i < new_size && i < old_size; i++)
+		p[i] = ptrc[i];
+	free(ptr);
+	return (p);
+}
