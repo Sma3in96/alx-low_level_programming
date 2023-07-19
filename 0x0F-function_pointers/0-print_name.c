5e46d13f1775b760c73e84ab179ea915847afb5d
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-19 05:10:48 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name given
+ * @name: str
+ * @f: function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name)
+		(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+this direct is for Function pointers
