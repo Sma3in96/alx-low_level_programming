31895f43f8e16ee515df3dc9f947384a891a7e72
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-19 05:31:21 -0700

    task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - exe f depends on the parameter
+ * @array: array
+ * @size: size of array
+ * @action: funtion to use
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
