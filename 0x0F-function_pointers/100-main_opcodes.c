73b5744e763cd6fda7cd40c9d02bf7b9258913fe
Sma3in96 <oumouss.ismail5@gmail.com>
2023-07-19 12:23:04 -0700

    taskadv

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	char *p = (char *)main;
+	int i, int a;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	a = atio(argv[1]);
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < a; i++)
+	{
+		printf("%02x", *p);
+		p++;
+		if (i !=  a - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0)
+}
