f265ad7e41a4183f04aac984c10e39db6ef57bd8
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-01 16:19:24 -0700

    task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -23,7 +23,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	nnode->next = NULL;
 	find = *head;
 	if (find ==  NULL)
-		*head = new;
+		*head = nnode;
 	else
 	{
 		while (find->next != NULL)
