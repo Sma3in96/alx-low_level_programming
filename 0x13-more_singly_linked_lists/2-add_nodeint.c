3171481dbea99bc5217151f9465dd6bea5605aad
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-01 17:26:43 -0700

    task3

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * add_nodeint - add to the end
+ * @h: list
+ * @n: int to add
+ * Return: head of list
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * add_nodeint_end - add to the end
+ * @h: list
+ * @n: int to add
+ * Return: head of list
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *token;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	token = *head;
+	if (token == NULL)
+		*head = new;
+	else
+	{
+		while (token->next)
+			token = token->next;
+		token->next = new;
+	}
+	return (*head);
+}
