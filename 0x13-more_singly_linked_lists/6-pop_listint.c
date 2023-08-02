f8a4a441e18768ae7f4107a3c96c1b2a6a9e36dd
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-02 15:12:39 -0700

    task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * pop_listint - pop a head
+ * @head: head
+ *
+ * Return: int or 0
+ */
+int pop_listint(listint_t **head)
+{
+	int j;
+	listint_t *temp;
+
+	if (head == NULL)
+		return (0);
+	else
+	{
+		temp = *head;
+		j = head->n;
+		head = head->next;
+		free(temp);
+		return (j);
+	}
+}
