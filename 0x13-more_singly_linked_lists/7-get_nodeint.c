eb94e3211c4116a03d7d54bfd0a5985dcc0b2eb2
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-02 15:33:26 -0700

    task7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index -  get
+ * @head: head
+ * index: index
+ * Return: pointer to index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < index && head)
+	{
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
