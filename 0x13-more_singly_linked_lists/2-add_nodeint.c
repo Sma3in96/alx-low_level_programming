ebd5c237796fdf678323280538e4350004aea5b7
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-01 17:31:42 -0700

    fixed header

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 /**
  * add_nodeint - add to the end
  * @h: list
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 /**
  * add_nodeint_end - add to the end
  * @h: list
