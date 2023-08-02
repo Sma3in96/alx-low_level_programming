127794520dc84195b1db3fe47d16dda07c8b16fc
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-02 14:40:36 -0700

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - plus free
+ * @head: head of linked lists
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *joe, *current;
+
+	if (head)
+	{
+		current = *head;
+		joe = current
+		while (joe)
+		{
+			current = current->next;
+			free(joe);
+			joe = current;
+		}
+		*head = NULL;
+	}
+}
