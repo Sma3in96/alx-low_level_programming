cad524c1a11579f25a15f947157a063006b076f8
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-03 13:39:25 -0700

    task5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - number of bits
+ * @n: 1st int
+ * @m: 2nd int
+ * Return: number of flips
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int xor, i, flips = 0;
+
+	xor = n ^ m;
+	for (i = 0; i < 64; i++)
+	{
+		if (xor & 1)
+			flips++;
+		xor = xor >> 1;
+	}
+	return (flips);
+}
