85ee52e967baf7422750bc93afde96226a56381d
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-03 13:59:35 -0700

    task5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,13 +7,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int xor, flips = 0;
+	unsigned long int xor, flips = 0;
 
 	xor = n ^ m;
 	while (xor)
 	{
-		flips ++;
-		xor &= (xor - 1);
+		flips += xor & 1;
+		xor = xor >> 1;
 	}
 	return (flips);
 }
