6ca225f7a284e4f55c19d7ddbdbe154fa4b8fafe
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-03 13:25:20 -0700

    task4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * clear_bit - set to 0
+ * @n: int
+ * @index: index
+ * Return: new value
+ */
+int clear_bit(unsigned long int *n, unsigned int index);
+{
+	unsigned int temp;
+
+	if (index > 63)
+		return (-1);
+	temp = ~(1 << index);
+	*n = (*n & temp);
+	return (*n);
+}
