c19fbb85d609af2753c727a9ad15aa6fd0d2a8be
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-03 13:16:10 -0700

    task3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * set_bit - set a bit
+ * @n: int
+ * @index: index
+ * Return: 1 or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int temp = 0;
+
+	if (index > 63)
+		return (-1);
+	temp = 1 << index;
+	n = (*n | temp);
+	return (1);
+}
