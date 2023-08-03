bb96bd39c071c6761211147066ba269a47266981
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-03 12:14:34 -0700

    task2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int get_bit - get at index
+ * @n: int
+ * @index: index
+ *
+ * Return int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (n == 0)
+		return (0);
+	if (index > 63)
+		return (-1);
+	return ((n >> i) | 1);
+}
