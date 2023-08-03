366f9f0186ec6d117e8f3492fa59c2fa4ea1b3d9
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-03 11:57:16 -0700

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * print_binary - print base 2 representation
- * @n: int 
+ * @n: int
  * Return: void
 */
 void print_binary(unsigned long int n)
 {
-    if (n)
-    {
-        if (n >> 1)
-            print_binary(n >> 1);
-        _putchar((n & 1) + '0');
-    }
-    else
-        _putchar('0');
+	if (n)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+		}
+	else
+		_putchar('0');
 }
