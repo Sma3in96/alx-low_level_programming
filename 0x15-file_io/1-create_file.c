8dfef6c25c6f5b476b86256b57aed09d0a351106
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-07 11:11:14 -0700

    task1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * create_file - create a new file
+ * @filename: fname
+ * @text_content: text to write
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i, file, wfile;
+
+	if (filename == NULL)
+		return (-1);
+	
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+		wfile = write(file, text_content, i);
+		if (wfile == -1)
+			return (-1);
+	}
+	close(file);
+	return (1);
+}
