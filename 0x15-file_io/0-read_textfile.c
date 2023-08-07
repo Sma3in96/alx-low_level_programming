fd99084b32b9cf743e1ff9123a6ad8bbe490e046
Sma3in96 <oumouss.ismail5@gmail.com>
2023-08-07 09:57:06 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,5 +7,25 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *p;
+	ssize_t chars, file; 
+	ssize_t chars2;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);	
 	return (0);
+	p = malloc(letters * sizeof(char));
+	chars = read(file, p, letters);
+	if (chars == -1)
+		return (0);
+	chars2 = write(STDOUT_FILENO, p, chars);
+	if (chars2 == -1)
+		return (0);
+	chars = chars2;
+	close (file);
+	free(p);
+	return (chars);
 }
